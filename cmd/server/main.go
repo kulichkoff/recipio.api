a67258ea7ca4951956e6d0919620b270fb41fc4d
Daniel Kulichkov <danchick03@gmail.com>
2025-03-16 23:45:27 +0300

    feat(server): connect default middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/labstack/echo/v4"
+	emiddleware "github.com/labstack/echo/v4/middleware"
 	"go.uber.org/zap"
 	"recip.io/api/internal/db"
 )
@@ -22,6 +23,23 @@ func main() {
 
 	e := echo.New()
 	e.HideBanner = true
+
+	e.Use(emiddleware.RequestLoggerWithConfig(emiddleware.RequestLoggerConfig{
+		LogURI:     true,
+		LogLatency: true,
+		LogStatus:  true,
+		LogValuesFunc: func(c echo.Context, v emiddleware.RequestLoggerValues) error {
+			logger.Info("request",
+				zap.String("URI", v.URI),
+				zap.Int("status", v.Status),
+			)
+
+			return nil
+		},
+	}))
+	e.Use(emiddleware.Recover())
+	e.Use(emiddleware.Gzip())
+	e.Use(emiddleware.CORSWithConfig(emiddleware.DefaultCORSConfig))
 
 	// TODO add port config
 	port := 8080
